4219b627dae13878cba3998c75d9940bb1e1394d
Daniel Fisher <danielg16fisher@gmail.com>
2025-05-27 12:01:15 +0300

    Paratrooper

--- CommandoProject/Paratrooper.cs
+++ CommandoProject/Paratrooper.cs
@@ -4,6 +4,15 @@ namespace Commando.Models
 {
     public class Paratrooper : Commando
     {
-        public Paratrooper(string name, List<string> tools) : base()
+        public Paratrooper(string name, List<string> tools) : base(name,tools)
+        {
+           
+        }
+
+        public void Parachute()
+        {
+            Console.WriteLine($"{CodeName} is parachuting");
+            CurrentStatus = "In the air";
+        }
     }
 }
