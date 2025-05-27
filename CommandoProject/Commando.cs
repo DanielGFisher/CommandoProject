1194c15dcc7b98a0e05d7c6e7ef39859b61b6588
Daniel Fisher <danielg16fisher@gmail.com>
2025-05-27 11:02:47 +0300

    Commando Complete

--- CommandoProject/Commando.cs
+++ CommandoProject/Commando.cs
@@ -1,9 +1,38 @@
 ﻿using System;
 
-namespace Commando
+namespace Commando.Models
 {
     public class Commando
     {
+        public string Name { get; set; }
+        public string CodeName { get; set; }
+        public List<string> Tools { get; set; }
+        public string CurrentStatus { get; set; }
 
+        public Commando(string name, string codename, List<string> tools, string status)
+        {
+            Name = name;
+            CodeName = codename;
+            Tools = tools;
+            CurrentStatus = status;
+        }
+
+        public void Walk()
+        {
+            Console.WriteLine($"{CodeName} is walking");
+            CurrentStatus = "Walking";
+        }
+
+        public void Hide()
+        {
+            Console.WriteLine($"{CodeName} is hiding");
+            CurrentStatus = "Hiding";
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine($"{CodeName} is attacking");
+            CurrentStatus = "Hiding";
+        }
     }
 }
